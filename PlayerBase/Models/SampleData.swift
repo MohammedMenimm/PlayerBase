cd25aa69d791df6a5c43f9de957bb3a4830242c2
MohammedMenimm <Mohammed.Meniim@gmail.com>
2024-10-04 15:19:40 +0200

    Added sample data for Fotball players

---
+++ PlayerBase/Models/SampleData.swift
@@ -0,0 +1,9 @@
+// Models/SampleData.swift
+import Foundation
+
+let players = [
+    FootballPlayer(name: "Lionel Messi", position: "Forward", marketValue: "$50M"),
+    FootballPlayer(name: "Cristiano Ronaldo", position: "Forward", marketValue: "$30M"),
+    FootballPlayer(name: "Kevin De Bruyne", position: "Midfielder", marketValue: "$70M"),
+    FootballPlayer(name: "Virgil van Dijk", position: "Defender", marketValue: "$60M"),
+]
