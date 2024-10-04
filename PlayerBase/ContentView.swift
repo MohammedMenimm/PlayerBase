ceb62c8c3c122dbe77877404c2db309da707967b
MohammedMenimm <Mohammed.Meniim@gmail.com>
2024-10-04 13:45:19 +0200

    Initial Commit

---
+++ PlayerBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayerBase
+//
+//  Created by Mohammed Menim on 2024-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayerBase/PlayerBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerBaseApp.swift
+//  PlayerBase
+//
+//  Created by Mohammed Menim on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct PlayerBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
